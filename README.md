1f3ca5e4f23702720333bcc66a6e1652ed78fd60
Kyle <kylek918@icloud.com>
2018-08-10 13:01:32 +0200

    added readme and updated search function

---
+++ README.md
@@ -0,0 +1,11 @@
+**This README documents what the project is and what skills I demonstrated during it's conception.**
+
+An interactive photo gallery with a lightbox and search function:
+
+* Structured and styled according to a mockup
+
+* Responsively designed through breakpoints
+
+* Hand coded a search function
+
+* Used a jQuery plugin to create lightbox functionality
--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-//Search function
+//SEARCH FUNCTION
 
+//links to the search input and image data in HTML.
 var imgSearch = document.getElementById("myInput");
 var imgDataTitle = document.querySelectorAll("a[data-lc-caption]");
-
+//listens for keyboard input then performs the function
 imgSearch.addEventListener("keyup", function(filtering){
-
+//converts search input to uppercase values
 	let filtered = imgSearch.value.toUpperCase();
-
+//"i" is less than the length of the image data and increments through it
 	for (var i = 0; i < imgDataTitle.length; i++){
-
+//increments through image data and retrieves data on input
 		var title = imgDataTitle[i].getAttribute("data-lc-caption");
-
+//displays image based on image data
 		if (title.toUpperCase().includes(filtered)) {
-			imgDataTitle[i].style.display = "inline";
+			imgDataTitle[i].style.display = "inline";//if the input is Caps or not display image
 		} else {
-			imgDataTitle[i].style.display = "none";
+			imgDataTitle[i].style.display = "none";//else dont display anything
 		}
 	}
 });
